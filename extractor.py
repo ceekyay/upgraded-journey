b27074a3f1d627ddb224f03af6ac0678a22ba8c6
ceekyay <chaitanya-kovuri@uiowa.edu>
2018-10-22 13:16:50 -0700

    Extractor.py fully functional
    Removed duplicate files

--- extractor.py
+++ extractor.py
@@ -1,3 +1,4 @@
+import time
 import gzip
 import os.path
 import fnmatch
@@ -6,64 +7,81 @@ import pandas as pd
 from pandas.io.json import json_normalize
 import glob
 
-root_dir = '/Users/CK/Desktop/jsonExtractor/2016'
-pattern = '*.json.gz'
-
-for path, subdirs, files in os.walk(root_dir):
-    for filename in fnmatch.filter(files, pattern):
-        #print (os.path.join(path, filename))
-        # dest_dir = "/Users/CK/Desktop/jsonExtractor/"+os.path.join(filename)[:-3]
-        dest_dir = "/Users/CK/Desktop/jsonExtractor/json/"+os.path.join(filename)[:-3]
-        with gzip.open(os.path.join(path, filename),'rb') as infile:
-            with open(dest_dir, 'wb') as outfile:
-                for line in infile:
-                    outfile.write(line)
+BASE_FILEPATH = "/Users/CK/Documents/8K/upgraded-journey/"
+data_source = BASE_FILEPATH + "Data/"
+data_destination = BASE_FILEPATH + "Data/json/"
+pattern = "*.json.gz"
+concatenated_json = {}
+dataframe = pd.DataFrame([])
 
 
-def flatten_json(y):
-    out = {}
-
-    def flatten(x, name=''):
-        if type(x) is dict:
-            for a in x:
-                flatten(x[a], name + a + '_')
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + '_')
-                i += 1
-        else:
-            out[name[:-1]] = x
-
-    flatten(y)
-    return out
+def unpack():
+    """
+    :return:
+    """
+    for path, subdirs, files in os.walk(data_source):
+        for filename in fnmatch.filter(files, pattern):
+            dest_dir = "/Users/CK/Documents/8K/upgraded-journey/Data/json/" \
+                       + os.path.join(filename)[:-3]
+            with gzip.open(os.path.join(path, filename), 'rb') as infile:
+                with open(dest_dir, 'wb') as outfile:
+                    for line in infile:
+                        outfile.write(line)
 
 
-# Create a new python dataframe
-df_combined = pd.DataFrame([])
-# Count number of text files in Data folder into a variable
-Counter = len(glob.glob1("/Users/CK/Desktop/jsonExtractor/json/","*.json"))
-print(Counter)
-i = 1
-for infile in glob.glob1("/Users/CK/Desktop/jsonExtractor/json/","*.json"):
-    if i <= (Counter):
-        data_parsed = json.loads(open("/Users/CK/Desktop/jsonExtractor/json/"+infile).read())
-        Object = data_parsed['Records']
-        j = 0
-        while j<len(Object):
-            print("_______________")
-            print(Object[j])
-            flat = flatten_json(Object[j])
-            #print(flat)
-            df = json_normalize(flat)
-            #print(df)
-            df_combined = df_combined.append(df)
-            print(j)
-            j += 1
-            print(j)
-
-df_combined.to_csv("/Users/CK/Desktop/jsonExtractor/Newfile.csv", index=False)
+def gen_new_concatenated_json(data):
+    """
+    :param data:
+    :return:
+    """
+    global concatenated_json
+    concatenate_child_attributes_to_parent(child_data=data)
+    return concatenated_json
 
 
-print(df_combined.shape)
+def concatenate_child_attributes_to_parent(child_data=None,
+                                           parent_attribute=""):
+    """
+    :param child_data:
+    :param parent_attribute:
+    :return:
+    """
+    global concatenated_json
+    if isinstance(child_data, dict):
+        for attribute in child_data:
+            # Recursion: If even the "attribute" in child_data has more nesting.
+            concatenate_child_attributes_to_parent(child_data[attribute],
+                                                   parent_attribute
+                                                   + attribute + "_")
+    elif isinstance(child_data, list):
+        for index, attribute in enumerate(child_data):
+            concatenate_child_attributes_to_parent(attribute,
+                                                   parent_attribute
+                                                   + str(index) + "_")
+    else:
+        concatenated_json[parent_attribute[:-1]] = child_data
 
+
+def write_to_csv(csv_name):
+    csv_location = BASE_FILEPATH + csv_name + ".csv"
+    dataframe.to_csv(csv_location, index=False)
+
+
+def __main__():
+    start = time.time()
+    unpack()
+    for infile in glob.glob1(data_destination, "*.json"):
+        data_parsed = json.loads(open(data_destination + infile).read())
+        parent_data = data_parsed['Records']
+        for count in range(len(parent_data)):
+            modified_data = gen_new_concatenated_json(parent_data)
+            dataframe.append(json_normalize(modified_data))
+    end_collection = time.time()
+    print("DATA COLLECTION TIME: ", end_collection-start)
+    write_to_csv("main_data")
+    end = time.time()
+    print("DATA WRITE TIME: ", end_collection-end)
+
+
+if __name__ == __main__():
+    __main__()
